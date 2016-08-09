d8ee24f9189a3cb5f764b3d38e556752170e0ad9
Alana Gilston <abluescarab@users.noreply.github.com>
2016-08-09 03:51:05 -0700

    Initial commit

---
+++ MPlayer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Terraria;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using System.IO;
+
+namespace MultiLure {
+    public class MPlayer : ModPlayer {
+        private byte lureCount = 1;
+
+        public byte LureCount {
+            get { return lureCount; }
+            set {
+                if(value < 1) {
+                    lureCount = 1;
+                }
+                else if(value > MultiLure.MAX_LURES) {
+                    lureCount = MultiLure.MAX_LURES;
+                }
+                else {
+                    lureCount = value;
+                }
+            }
+        }
+
+        public override bool Autoload(ref string name) {
+            return true; 
+        }
+
+        public override void SaveCustomData(BinaryWriter writer) {
+            writer.Write(lureCount);
+        }
+
+        public override void LoadCustomData(BinaryReader reader) {
+            try {
+                lureCount = reader.ReadByte();
+            }
+            catch(EndOfStreamException) {
+                lureCount = 1;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+MultiLure is a mod that adds a configurable number of lures while fishing.
+
+* Includes Cheat Sheet and HERO's Mod integration (hotkeys work with or without integration)
+* Press [ to remove a lure
+* Press ] to add a lure
+
+## Credits
+* Nomad000 for the suggestion in the Terraria Forums
+* jopojelly for Cheat Sheet
+* HERO for HERO's Mod
