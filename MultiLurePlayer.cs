196e952942fed656fa1ca9f14aac8e406446baff
Alana Gilston <abluescarab@users.noreply.github.com>
2019-04-15 03:18:05 -0700

    Cleaned code; changed LureCount to int; switched to C#6

--- MultiLurePlayer.cs
+++ MultiLurePlayer.cs
@@ -1,51 +1,20 @@
-﻿using System;
-using Terraria;
-using Terraria.ModLoader;
-using Microsoft.Xna.Framework;
-using System.IO;
+﻿using Terraria.ModLoader;
 using Terraria.ModLoader.IO;
 
 namespace MultiLure {
     public class MultiLurePlayer : ModPlayer {
         private const string LURE_COUNT = "lurecount";
-        private byte lureCount = 1;
 
-        public byte LureCount {
-            get { return lureCount; }
-            set {
-                if(value < 1) {
-                    lureCount = 1;
-                }
-                else if(value > MultiLure.MAX_LURES) {
-                    lureCount = MultiLure.MAX_LURES;
-                }
-                else {
-                    lureCount = value;
-                }
-            }
-        }
-
-        public override bool Autoload(ref string name) {
-            return true; 
-        }
+        public int LureCount { get; set; } = 1;
 
         public override TagCompound Save() {
             return new TagCompound {
-                { LURE_COUNT, lureCount }
+                { LURE_COUNT, (byte)LureCount }
             };
         }
 
         public override void Load(TagCompound tag) {
-            lureCount = tag.GetByte(LURE_COUNT);
-        }
-
-        public override void LoadLegacy(BinaryReader reader) {
-            try {
-                lureCount = reader.ReadByte();
-            }
-            catch(EndOfStreamException) {
-                lureCount = 1;
-            }
+            LureCount = tag.GetByte(LURE_COUNT);
         }
     }
 }
