8f1cb7f9273df90721837104408d2f5e00a57f07
Alana Gilston <abluescarab@users.noreply.github.com>
2023-08-11 01:08:04 -0700

    Fixed copper fishing line properties

--- Items/CopperFishingLine.cs
+++ Items/CopperFishingLine.cs
@@ -5,8 +5,8 @@ namespace MultiLure.Items {
     public class CopperFishingLine : DeprecatedItem {
         public override string OriginalName => "CopperFishingLine";
         public override string AlternativeName => "TinFishingLine";
-        public override string OriginalDisplayName => "{$Mods.MultiLure.Items.CopperFishingLine}";
-        public override string AlternativeDisplayName => "{$Mods.MultiLure.Items.TinFishingLine}";
+        public override string OriginalType => "{$Mods.MultiLure.Items.CopperFishingLine}";
+        public override string AlternativeType => "{$Mods.MultiLure.Items.TinFishingLine}";
         public override int Lures => 1;
 
         protected override ReplacementItem[] Replacements => new ReplacementItem[] {
